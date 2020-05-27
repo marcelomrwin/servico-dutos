051b134d79b80e525bc7abea1564b75e90453f55
Marcelo Sales <marcelo.d.sales@gmail.com>
2020-05-27 02:37:46 -0300

    update project

--- src/main/java/br/com/comgas/router/CamelRouter.java
+++ src/main/java/br/com/comgas/router/CamelRouter.java
@@ -1,6 +1,12 @@
 package br.com.comgas.router;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jbpm.JBPMConstants;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +27,31 @@ public class CamelRouter extends RouteBuilder {
 		rest("/dutos").description("Localização do duto pelo valor de geolocalização Latitude/Longitude")
 				.get("/{Latitude}/{Longitude}").outType(Dutos.class).route().routeId("dutos-api").to("direct:dutos");
 
-		from("direct:dutos").id("dutos_mysql").description("Consulta de dutos por dados de Geo Localização").streamCaching()
+		rest("/calcular").description("Calcular Priorização").get("/{Latitude}/{Longitude}/{Distancia}/{Metodo}")
+				.route().routeId("calcular-api").to("direct:calcular");
+
+		from("direct:dutos").id("dutos_mysql").description("Consulta de dutos por dados de Geo Localização")
+				.streamCaching()
 				.to("sql:SELECT * FROM DUTOS WHERE LATITUDE =:#Latitude AND LONGITUDE =:#Longitude?outputType=SelectOne&outputClass=br.com.comgas.beans.Dutos");
+
+		from("direct:calcular").process(new Processor() {
+
+			@Override
+			public void process(Exchange ex) throws Exception {
+
+				Map<String, Object> params = new HashMap<>();
+				
+				params.put("Latitude",ex.getIn().getHeader("Latitude", Double.class));
+				params.put("Longitude",ex.getIn().getHeader("Longitude", Double.class));
+				params.put("Distancia", ex.getIn().getHeader("Distancia", Float.class));
+				params.put("Metodo",ex.getIn().getHeader("Metodo", String.class));
+				
+				ex.getMessage().setHeader(JBPMConstants.PARAMETERS, params);
+			}
+		}).setHeader(JBPMConstants.PROCESS_ID, constant("IPC.Processo_Priorizacao")).to(
+				"jbpm:http://localhost:8080/kie-server/services/rest/server?userName=pamAdmin&password=S3cr3tK3y#&deploymentId=ipc_1.0.0-SNAPSHOT")
+				.log("${body}");
+
 	}
 
 }
